6536983b6305c04cd78a7805ce573a5d70b75b31
MaleekNoob <maleekhussainali456@gmail.com>
2023-02-02 14:48:30 +0500

    commit 1

--- Source.cpp
+++ Source.cpp
@@ -1,30 +1,68 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
-int* reverseLinkedList(int* list);
+int* reverseLinkedList(int* array, int size);
 
 int* increaseDynamicArray(int* array, int &size);
 
 int main() {
-	int size = 0;
+	int size = 0, iter = 0;
 	int* list = new int[NULL];
 	char user;
 	do
 	{
-		increaseDynamicArray(list, size);
+		list = increaseDynamicArray(list, size);
 		cout << "\nEnter number: ";
-		cin >> list[i];
+		cin >> list[iter];
 		cout << "\nagain?";
 		cin >> user;
+		iter++;
 
 	} while (user == 'y');
+
+	for (int i = 0; i < size; i++) {
+
+		if (i == 0) {
+			cout << endl << "List you entered is: ";
+			cout << "\nList: {" << list[i];
+		}
+		else {
+			cout << " ," << list[i];
+		}
+		if (i == (size - 1)) {
+			cout << "}";
+		}
+	}
+
+	list = reverseLinkedList(list, size);
+
+	for (int i = 0; i < size; i++) {
+
+		if (i == 0) {
+			cout << endl << "Reversed List is: ";
+			cout << "\nList: {" << list[i];
+		}
+		else {
+			cout << " ," << list[i];
+		}
+		if (i == (size - 1)) {
+			cout << "}";
+		}
+	}
 
 	return 0;
 }
 
-int* reverseLinkedList(int* list) {
-	//a -> b -> c
-	//c -> b -> a
+int* reverseLinkedList(int* array, int size) {
+	int* reverseArray = new int[size];
+
+	for (int i = 0; i < size; i++) {
+		reverseArray[(size - i) - 1] = array[i];
+	}
+
+	delete[] array;
+	return reverseArray;
 }
 
 int* increaseDynamicArray(int* array, int& size) {
